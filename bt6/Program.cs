335a44bcb5d077100254bdd4f49a83e52535f541
tanphatngo12 <tanphat2002125@gmail.com>
2023-03-20 12:15:08 +0700

    edit code

--- bt6/Program.cs
+++ bt6/Program.cs
@@ -16,13 +16,14 @@ namespace bt6
             int thang = int.Parse(Console.ReadLine());
             Console.WriteLine("Nhap nam: ");
             int nam = int.Parse(Console.ReadLine());
-            int ngayhomqua = ngay--;
-            int ngaymai = ngay++;
+            int ngayhomqua = ngay-1;
+            int ngaymai = ngay+1;
             Console.WriteLine("<----------------------------------->");
             Console.WriteLine("Ngay hom qua la {0}/{1}/{2}", ngayhomqua, thang, nam);
             Console.WriteLine("Ngay hom nay la {0}/{1}/{2}", ngay, thang, nam);
             Console.WriteLine("Ngay mai la {0}/{1}/{2}", ngaymai, thang, nam);
             Console.WriteLine("<----------------------------------->");
+            Console.ReadLine();
         }
     }
 }
