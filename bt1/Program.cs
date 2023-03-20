bd8092758ad3b2f7aec5759479e795c7ff29fab0
Phat Ngo <Phat Ngo@DESKTOP-7K29C86>
2023-03-20 10:53:01 +0700

    Add project files.

---
+++ bt1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so nguyen a ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so nguyen b ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("so a la {0}, so b la {1}", a, b);
+            float thuong = (float)a / b;
+            int tong = a + b;
+            int hieu = a - b;
+            int tich = a * b;
+            Console.WriteLine("tong la: {0}", tong);
+            Console.WriteLine("hieu la: {0}", hieu);
+            Console.WriteLine("tich la: {0}", tich);
+            Console.WriteLine("thuong la:{0} ", thuong);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bt2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap chieu dai: ");
+            int cd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap chieu rong: ");
+            int cr = int.Parse(Console.ReadLine());
+            int cv = (cd + cr) * 2;
+            int dt = cd * cr;
+            Console.WriteLine("Chu vi hinh chu nhat la: " + cv);
+            Console.WriteLine("Dien tich hinh chu nhat la: " + dt);
+            Console.ReadLine();
+        }
+    }
+}
